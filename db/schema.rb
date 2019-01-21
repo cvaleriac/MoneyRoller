9bfc0e5b2eca771b29d7fcf2905637cbf63d4763
valeria campilongo <cvaleriac@hotmail.it>
2019-01-21 22:44:38 +0000

    index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_201955) do
+ActiveRecord::Schema.define(version: 2019_01_21_223911) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
