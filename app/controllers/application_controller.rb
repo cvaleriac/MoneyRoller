d00cd801a2b9be3ecce34a945e6dc1975a577fac
valeria campilongo <cvaleriac@hotmail.it>
2019-01-24 16:35:06 +0000

    failure and account added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,53 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/login' do
+  get '/signup' do
+    erb :signup
+  end
+
+  post "/signup" do
+    if params[:username].empty?
+      redirect to '/failure'
+    end
+
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect '/login'
+    else
+      redirect '/failure'
+    end
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
+
+
+  get "/login" do
     erb :login
   end
 
-  get '/signup' do
-    erb :signup
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end
+  end
+
+
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
   end
 
 
