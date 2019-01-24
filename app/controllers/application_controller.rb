8977625534901db98bf02b60c5ae793eb48961a3
valeria campilongo <cvaleriac@hotmail.it>
2019-01-24 22:32:25 +0000

    editing controllers and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "golfclubsaregreat"
+    set :session_secret, "moneyroller"
   end
 
 
@@ -15,5 +15,51 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/signup' do
+    erb :signup
+  end
+
+  post "/signup" do
+    if params[:username].empty?
+      redirect to '/failure'
+    end
+
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect '/login'
+    else
+      redirect '/failure'
+    end
+  end
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
+
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
 
 end
---
+++ app/controllers/rollovers_controller.rb
@@ -0,0 +1,4 @@
+class RolloversController < ApplicationController
+
+
+end
