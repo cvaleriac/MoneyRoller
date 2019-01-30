930d5daf58329250a332ecef8e85ce1dbc178ff4
valeria campilongo <cvaleriac@hotmail.it>
2019-01-30 16:52:05 +0000

    editing usercontroller not working

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,46 +1,48 @@
 class UserController < ApplicationController
 
-  get '/signup' do
-  if Helpers.is_logged_in?(session)
-    redirect to '/rollovers'
-  end
-  erb :'/users/create_user'
+  get "/signup" do
+  erb :'/users/signup'
 end
 
-post '/signup' do
-  if !(params.has_value?(""))
-    user = User.create(params)
-    session["user_id"] = user.id
-    redirect to '/rollovers'
+post "/signup" do
+  if params[:username].empty?
+    redirect to '/failure'
+  end
+
+  user = User.new(:username => params[:username], :password => params[:password])
+  if user.save
+    redirect '/login'
   else
-    redirect to '/signup'
+    redirect '/failure'
   end
 end
 
-get '/login' do
-  if Helpers.is_logged_in?(session)
-    redirect to '/rollovers'
-  end
+get "/login" do
   erb :'/users/login'
 end
 
 post '/login' do
-user = User.find_by(:username => params[:username])
-  if user && user.authenticate(params[:password])
-    session["user_id"] = user.id
-    redirect to "/rollovers"
-  else
-    redirect to "/login"
+  user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session["user_id"] = user.id
+      redirect to "/user_rollovers"
+    else
+      redirect to "/login"
+    end
   end
+
+get '/user_rollovers' do
+  @user = User.find(session[:user_id])
+  erb :'/users/user_rollovers'
 end
 
-get '/logout' do
-  if Helpers.is_logged_in?(session)
-    session.clear
-  else
-    redirect to '/'
-  end
-  redirect to '/login'
+get "/failure" do
+  erb :'/users/failure'
+end
+
+get "/logout" do
+  session.clear
+  redirect "/"
 end
 
 end
