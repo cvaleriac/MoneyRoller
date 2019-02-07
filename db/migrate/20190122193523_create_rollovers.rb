edc1548b968cc2a323c8089a11efd89f7c1c829a
valeria campilongo <cvaleriac@hotmail.it>
2019-02-07 21:59:55 +0000

    updated migration

--- db/migrate/20190122193523_create_rollovers.rb
+++ db/migrate/20190122193523_create_rollovers.rb
@@ -1,11 +1,12 @@
 class CreateRollovers < ActiveRecord::Migration[5.2]
   def change
     create_table :rollovers do |t|
-      t.string :rollover_id
       t.string :rollover_type
       t.string :origin_bank
       t.string :destination_bank
-      t.string :integer
+      t.string :amount
+      t.integer :user_id
+      t.timestamps
   end
  end
 end
