12606a0800f1217c2da628c39346f804bdfcffd6
valeria campilongo <cvaleriac@hotmail.it>
2019-02-14 15:59:32 +0000

    set New.erb form

--- app/controllers/rollover_controller.rb
+++ app/controllers/rollover_controller.rb
@@ -1,13 +1,23 @@
 class RolloversController < ApplicationController
 
-    get "/rollovers" do
-      @rollovers = Rollover.all
-      erb :'/rollovers/index'
+
+    get "/rollover/new" do
+    erb :'/rollovers/new'
     end
+
+    post "/rollovers" do
+
+    end
+
 
     get "/rollover/:id" do
     @rollover = Rollover.find(params[:id])
     erb :'/rollovers/show'
+    end
+
+    get "/rollovers" do
+      @rollovers = Rollover.all
+      erb :'/rollovers/index'
     end
 
 
